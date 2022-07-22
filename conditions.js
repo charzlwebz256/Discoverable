e7233630394dd036bdbac1d03ee6ede6b792da92
Lwebuga Charles <105212396+charzlwebz256@users.noreply.github.com>
2022-07-22 14:34:53 +0300

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,11 @@
+var number = 200;
+if (number > 100) {
+
+        console.log(" The number is less than hundred ");
+    }
+else if(number > 100) {
+    console.log(" The number is not greater than hundred");
+    }
+else {
+    console.log("None of the statements is true")
+}
